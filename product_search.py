1afa1717729e371cd35001d0a48adedc73da0d0e
Lorenzo Gentile <Lorenzo45@users.noreply.github.com>
2025-10-17 16:54:35 -0700

    Basic implementation

--- product_search.py
+++ product_search.py
@@ -1,46 +1,47 @@
 import os
+import argparse
 
 from parallel import Parallel
 from pydantic import BaseModel, Field
 from typing import Literal
 
-class CompanyOutput(BaseModel):
-    founding_date: str = Field(
-        description="The official founding date of the company in the format MM-YYYY"
+class ProductOutput(BaseModel):
+    title: str = Field(
+        description="The official name of the product"
     )
-    employee_count: Literal[
-        "1-10 employees",
-        "11-50 employees",
-        "51-200 employees",
-        "201-500 employees",
-        "501-1000 employees",
-        "1001-5000 employees",
-        "5001-10000 employees",
-        "10001+ employees"
-    ] = Field(
-        description="The range of employees working at the company. Choose the most accurate range possible and make sure to validate across multiple sources."
+    price: float = Field(
+        description="The price of the product to buy it outright (NOT monthly installment price)"
     )
-    funding_sources: str = Field(
-        description="A detailed description, containing 1-4 sentences, of the company's funding sources, including their estimated value."
+    currency: str = Field(
+        description="The currency of the product's price (e.g. USD, EUR, GBP)"
+    )
+    url: str = Field(
+        description="The URL where the product details are available"
     )
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", required=True, help="The product to search for")
+    args = parser.parse_args()
+
     client = Parallel(api_key=os.getenv("PARALLEL_API_KEY"))
 
     task_run = client.task_run.create(
-        input="United Nations",
+        input=args.query,
         task_spec={
           "output_schema":{
             "type":"json",
-            "json_schema":CompanyOutput.model_json_schema()
+            "json_schema":ProductOutput.model_json_schema()
           }
         },
         processor="core"
     )
-    print(f"Run ID: {task_run.run_id}")
+    print(f"Running Parallel task with run ID: {task_run.run_id}")
 
     run_result = client.task_run.result(task_run.run_id, api_timeout=3600)
-    print(run_result.output)
+    print("Run Result:", run_result.output)
+    print("-----")
+    print("Output:", run_result.output.content)
 
 if __name__ == "__main__":
     main()
